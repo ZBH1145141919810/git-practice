7ce230666f13d56f56332dcfc9aefb378c2af670
ZBH <19187898656@163.com>
2026-09-10 18:50:10 +0800

    工具提交02

--- Class4/数据计算小工具/计算工具.js
+++ Class4/数据计算小工具/计算工具.js
@@ -7,3 +7,26 @@ const courseList = [
 console.log("====绩点计算器====");
 console.log("课程数据：",courseList);
 
+const valid = courseList.filter(item=>{
+    return typeof item.score === "number" && item.score>=0 && item.score<=100;
+});
+
+const list = valid.map(c=>{
+    let g;
+    if(c.score>=90) g=4.0;
+    else if(c.score>=80) g=3.0;
+    else if(c.score>=70) g=2.0;
+    else if(c.score>=60) g=1.0;
+    else g=0;
+    return {gp:g*c.credit,cr:c.credit};
+});
+
+const res = list.reduce((a,b)=>{
+    a.sumGp += b.gp;
+    a.sumCr += b.cr;
+    return a;
+},{sumGp:0,sumCr:0});
+
+const gpa = res.sumCr===0 ? 0 : (res.sumGp/res.sumCr).toFixed(2);
+console.log("加权绩点：",gpa);
+
