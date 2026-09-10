d5e197dbea7e9c5925b8a2f9e4f3da30caa5546f
ZBH <19187898656@163.com>
2026-09-10 18:47:05 +0800

    工具提交01

---
+++ Class4/数据计算小工具/计算工具.js
@@ -0,0 +1,9 @@
+const courseList = [
+    {name:"高等数学",score:85,credit:4},
+    {name:"大学英语",score:78,credit:3},
+    {name:"Python程序设计",score:92,credit:3}
+];
+
+console.log("====绩点计算器====");
+console.log("课程数据：",courseList);
+
